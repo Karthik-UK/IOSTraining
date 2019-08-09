59bef3c0a05e5ddf1bf11574ce6250bff6a1cfa0
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-08-09 12:10:02 +0530

    First

---
+++ Appli/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Appli
+//
+//  Created by Karthik UK on 08/08/19.
+//  Copyright © 2019 Karthik UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label1: UILabel!
+   
+    @IBOutlet weak var Image1: UIImageView!
+    
+    
+    @IBOutlet weak var Label2: UILabel!
+    
+    
+    @IBOutlet weak var Text1: UITextField!
+    
+ 
+    @IBAction func Button1(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
