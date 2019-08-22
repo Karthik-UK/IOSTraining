4a77c791634bb0193da60e4b52d761f6488622aa
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-08-22 10:29:29 +0530

    tabbar improvements

--- YML-Application/YML-Application/ViewController.swift
+++ YML-Application/YML-Application/ViewController.swift
@@ -4,42 +4,15 @@ class ViewController: UIViewController{
     
     @IBOutlet weak var pageIndicator: UIPageControl!
     @IBOutlet weak var collectionView: UICollectionView!
-  
     @IBOutlet weak var buttonUI: UIButton!
-    
-    var images :[String] = ["","1","2","3","4"]
-    var heading:[String] = ["Hello","Molekule","The Home Depot","PayPal","State Farm"]
-    var imageLogo :[String] = ["","1logo", "2logo","3logo","4logo"]
-    var descriptn: [String] = ["We are a design and innovation agency, creating digital products and experiences that have a lasting impact.","The world's first intelligent air purifier, & the app putting clean air in people's hands. ",  "The ultimate power tool: A best-in-class digital experience for The Home Depot.","Payment giant goes mobile-by-design.","All things insurance, all things banking, all in one app."]
-    
-    
-    
-    
-    
-    
-    
 
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         buttonUI.layer.borderWidth = 2
         buttonUI.layer.borderColor = UIColor.black.cgColor
         configurePageControl()
-        
     }
-    
-    //function to configure pageindicator
-    func configurePageControl() {
-        pageIndicator.numberOfPages = images.count
-        pageIndicator.currentPage = 0
-        pageIndicator.tintColor = self.randomclr()
-        pageIndicator.pageIndicatorTintColor = .black
-        pageIndicator.currentPageIndicatorTintColor = self.randomclr()
-        view.addSubview(pageIndicator)
-    }
-    
+  
     //random colour generator
     func randomclr()->UIColor{
         let red = CGFloat(drand48())
@@ -47,10 +20,25 @@ class ViewController: UIViewController{
         let green = CGFloat(drand48())
         return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
-
-}
-
-
+    
+    //Structure for all static images,labels etc.
+    struct constantThings {
+        let images :[String] = ["","1","2","3","4"]
+        let headings:[String] = ["Hello","Molekule","The Home Depot","PayPal","State Farm"]
+        let imageLogos :[String] = ["","1logo", "2logo","3logo","4logo"]
+        let descriptions: [String] = ["We are a design and innovation agency, creating digital products and experiences that have a lasting impact.","The world's first intelligent air purifier, & the app putting clean air in people's hands. ",  "The ultimate power tool: A best-in-class digital experience for The Home Depot.","Payment giant goes mobile-by-design.","All things insurance, all things banking, all in one app."]
+    }
+    let const = constantThings()
+    
+    //function to configure pageindicator
+    func configurePageControl() {
+        pageIndicator.numberOfPages = const.images.count
+        pageIndicator.currentPage = 0
+        pageIndicator.tintColor = self.randomclr()
+        pageIndicator.pageIndicatorTintColor = .black
+        pageIndicator.currentPageIndicatorTintColor = self.randomclr()
+        view.addSubview(pageIndicator)
+    }}
 
 extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout,UIScrollViewDelegate{
     
@@ -62,16 +50,16 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource ,UI
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        return const.images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath as IndexPath) as? CollectionViewCell
         
-        cell?.imageView.image =  UIImage(named: self.images[indexPath.row])
-        cell?.headTitle.text = heading[indexPath.row]
-        cell?.logoImage.image = UIImage(named: self.imageLogo[indexPath.row])
-        cell?.descriptionTitle.text = descriptn[indexPath.row]
+        cell?.imageView.image =  UIImage(named: const.images[indexPath.row])
+        cell?.headTitle.text = const.headings[indexPath.row]
+        cell?.logoImage.image = UIImage(named: const.imageLogos[indexPath.row])
+        cell?.descriptionTitle.text = const.descriptions[indexPath.row]
         
         return cell ?? UICollectionViewCell()
     }
